89609802fd076919d20e417e256a09d4d181e520
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2022-01-29 18:57:43 +0200

    подключился к серверу , добавил env

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+// const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { DB_HOST } = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch(console.log(error => console.log(error.message)))
