35df5447c3f6a05f16c63e638c7dc4b0d874683d
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2022-02-02 01:11:12 +0200

    finish hw3

--- models/contact.js
+++ models/contact.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
 const contactSchema = Schema({
     name: {
@@ -19,4 +20,18 @@ const contactSchema = Schema({
   
 const Contact = model("contact", contactSchema);
 
-module.exports = Contact;
+const contactSchemaValidate = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'co', 'uk', 'ca'] } }),
+  phone: Joi.any(),
+  favorite: Joi.boolean(),
+});
+
+const contactSchemaValidateFavorite = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+module.exports = { Contact, schemas: {
+    add: contactSchemaValidate,
+    updateFavorite: contactSchemaValidateFavorite
+}};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const createError = require('http-errors');
-const Joi = require("joi");
+// const Joi = require("joi");
 
 // const contacts = require("../../models/contacts");
-const Contact = require("../../models/contact");
+const {Contact, schemas} = require("../../models/contact");
 
 const router = express.Router();
 
@@ -18,79 +18,109 @@ router.get('/', async (req, res, next) => {
   };
 });
 
-// router.get('/:contactId', async (req, res, next) => {
-//   try {
-//     const {contactId} = req.params;
-//     const result = await contacts.getContactById(contactId);
-//     if (!result) {
-//       // eslint-disable-next-line new-cap
-//       throw new createError(404, "Not found");
-//       // res.status(404).json({
-//       //   massage: "Not found"
-//       // })
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
-//   };
-// });
-
-// router.post('/', async (req, res, next) => {
-//   // res.json({ message: 'template message' })
-//   try {
-//     const { error } = contactSchemaValidate.validate(req.body);
-//     if (error) {
-//       // eslint-disable-next-line new-cap
-//       throw new createError(400, error.message)
-//     }
-//     const { name, email, phone } = req.body;
-//     const result = await contacts.addContact(name, email, phone);
-//     res.status(201).json(result)
-//   } catch (error) {
-//     next(error)
-//   };
-// });
-
-// router.put('/:contactId', async (req, res, next) => {
-//   // res.json({ message: 'template message' })
-//   try {
-//     const { contactId } = req.params;
-//     const { name, email, phone } = req.body;
-//     const { error } = contactSchemaValidate.validate(req.body);
-//     if (error) {
-//       // eslint-disable-next-line new-cap
-//       throw new createError(400, error.message)
-//     }
-//     const result = await contacts.updateContact(contactId, name, email, phone);
-//     if (!result) {
-//       // eslint-disable-next-line new-cap
-//       throw new createError(400, { "message": "missing fields" })
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error)
-//   }
-// });
-
-// router.delete('/:contactId', async (req, res, next) => {
-//   // res.json({ message: 'template message' })
-//   try {
-//     const { contactId } = req.params;
-//     const result = await contacts.removeContact(contactId);
-//     if (!result) {
-//       // eslint-disable-next-line new-cap
-//       throw new createError(404, "Not found");
-//     }
-//     res.status(200).json({ "message": "contact deleted" });
-//   } catch (error) {
-//     next(error)
-//   }
-// });
-
-const contactSchemaValidate = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'co', 'uk'] } }).required(),
-  phone: Joi.any().required(),
+router.get('/:contactId', async (req, res, next) => {
+  try {
+    const {contactId} = req.params;
+    // const result = await contacts.getContactById(contactId);
+    const result = await Contact.findById(contactId);
+    if (!result) {
+      // eslint-disable-next-line new-cap
+      throw new createError(404, "Not found");
+      // res.status(404).json({
+      //   massage: "Not found"
+      // })
+    }
+    res.json(result);
+  } catch (error) {
+    if (error.message.includes("Cast to ObjectId failed")) {
+      error.status = 404;
+    }
+    next(error);
+  };
 });
+
+router.post('/', async (req, res, next) => {
+  // res.json({ message: 'template message' })
+  try {
+    const { error } = schemas.add.validate(req.body);
+    if (error) {
+      // eslint-disable-next-line new-cap
+      throw new createError(400, error.message)
+    }
+    // const { name, email, phone } = req.body;
+    // const result = await contacts.addContact(name, email, phone);
+    const result = await Contact.create(req.body);
+    res.status(201).json(result)
+  } catch (error) {
+    next(error)
+  };
+});
+
+router.put('/:contactId', async (req, res, next) => {
+  // res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    // const { name, email, phone } = req.body;
+    const { error } = schemas.add.validate(req.body);
+    if (error) {
+      // eslint-disable-next-line new-cap
+      throw new createError(400, error.message)
+    }
+    // const result = await contacts.updateContact(contactId, name, email, phone);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+    if (!result) {
+      // eslint-disable-next-line new-cap
+      throw new createError(400, { "message": "missing fields" })
+    }
+    res.json(result);
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.patch('/:contactId/favorite', async (req, res, next) => {
+  // res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    // const { name, email, phone } = req.body;
+    const { error } = schemas.updateFavorite.validate(req.body);
+    if (error) {
+      // eslint-disable-next-line new-cap
+      throw new createError(400, error.message)
+    }
+    // const result = await contacts.updateContact(contactId, name, email, phone);
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+    if (!result) {
+      // eslint-disable-next-line new-cap
+      throw new createError(400, { "message": "missing field favorite" })
+    }
+    res.json(result);
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.delete('/:contactId', async (req, res, next) => {
+  // res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    // const result = await contacts.removeContact(contactId);
+    const result = await Contact.findByIdAndDelete(contactId);
+    if (!result) {
+      // eslint-disable-next-line new-cap
+      throw new createError(404, "Not found");
+    }
+    res.status(200).json({ "message": "contact deleted" });
+  } catch (error) {
+    next(error)
+  }
+});
+
+// const contactSchemaValidate = Joi.object({
+//   name: Joi.string().required(),
+//   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'co', 'uk', 'ca'] } }),
+//   phone: Joi.any(),
+//   favorite: Joi.boolean(),
+// });
 
 module.exports = router
