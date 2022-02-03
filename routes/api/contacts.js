11ec7d77f0b431f5923728a938a28779eee0a287
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2022-02-03 10:02:24 +0200

    fix замечаний

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const createError = require('http-errors');
-// const Joi = require("joi");
 
-// const contacts = require("../../models/contacts");
 const {Contact, schemas} = require("../../models/contact");
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  // res.json({ message: 'template message' })
   try {
-    // const result = await contacts.listContacts();
     const result = await Contact.find();
     res.json(result);
   } catch (error) {
@@ -21,14 +17,10 @@ router.get('/', async (req, res, next) => {
 router.get('/:contactId', async (req, res, next) => {
   try {
     const {contactId} = req.params;
-    // const result = await contacts.getContactById(contactId);
     const result = await Contact.findById(contactId);
     if (!result) {
       // eslint-disable-next-line new-cap
       throw new createError(404, "Not found");
-      // res.status(404).json({
-      //   massage: "Not found"
-      // })
     }
     res.json(result);
   } catch (error) {
@@ -40,15 +32,12 @@ router.get('/:contactId', async (req, res, next) => {
 });
 
 router.post('/', async (req, res, next) => {
-  // res.json({ message: 'template message' })
   try {
     const { error } = schemas.add.validate(req.body);
     if (error) {
       // eslint-disable-next-line new-cap
       throw new createError(400, error.message)
     }
-    // const { name, email, phone } = req.body;
-    // const result = await contacts.addContact(name, email, phone);
     const result = await Contact.create(req.body);
     res.status(201).json(result)
   } catch (error) {
@@ -57,16 +46,13 @@ router.post('/', async (req, res, next) => {
 });
 
 router.put('/:contactId', async (req, res, next) => {
-  // res.json({ message: 'template message' })
   try {
     const { contactId } = req.params;
-    // const { name, email, phone } = req.body;
     const { error } = schemas.add.validate(req.body);
     if (error) {
       // eslint-disable-next-line new-cap
       throw new createError(400, error.message)
     }
-    // const result = await contacts.updateContact(contactId, name, email, phone);
     const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
     if (!result) {
       // eslint-disable-next-line new-cap
@@ -79,16 +65,13 @@ router.put('/:contactId', async (req, res, next) => {
 });
 
 router.patch('/:contactId/favorite', async (req, res, next) => {
-  // res.json({ message: 'template message' })
   try {
     const { contactId } = req.params;
-    // const { name, email, phone } = req.body;
     const { error } = schemas.updateFavorite.validate(req.body);
     if (error) {
       // eslint-disable-next-line new-cap
       throw new createError(400, error.message)
     }
-    // const result = await contacts.updateContact(contactId, name, email, phone);
     const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
     if (!result) {
       // eslint-disable-next-line new-cap
@@ -101,10 +84,8 @@ router.patch('/:contactId/favorite', async (req, res, next) => {
 });
 
 router.delete('/:contactId', async (req, res, next) => {
-  // res.json({ message: 'template message' })
   try {
     const { contactId } = req.params;
-    // const result = await contacts.removeContact(contactId);
     const result = await Contact.findByIdAndDelete(contactId);
     if (!result) {
       // eslint-disable-next-line new-cap
@@ -115,12 +96,5 @@ router.delete('/:contactId', async (req, res, next) => {
     next(error)
   }
 });
-
-// const contactSchemaValidate = Joi.object({
-//   name: Joi.string().required(),
-//   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'co', 'uk', 'ca'] } }),
-//   phone: Joi.any(),
-//   favorite: Joi.boolean(),
-// });
 
 module.exports = router
